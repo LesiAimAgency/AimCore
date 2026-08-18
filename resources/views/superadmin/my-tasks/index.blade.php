ac235e6d2ea3a088711cd8602f39d73f40d274b6
lesi.techaim@gmail.com <lesi.techaim@gmail.com>
2026-08-18 17:38:00 +0700

    sửa comfim

--- resources/views/superadmin/my-tasks/index.blade.php
+++ resources/views/superadmin/my-tasks/index.blade.php
@@ -977,7 +977,7 @@
                         Quay lại
                     </button>
                     <button type="button" @click="confirmDeleteTask()" class="w-1/2 px-4 py-2.5 bg-red-600 hover:bg-red-700 text-white rounded-xl text-sm font-semibold transition-all shadow-md shadow-red-200">
-                        Xác nhận xóa
+                        Xác nhận 
                     </button>
                 </div>
             </div>
